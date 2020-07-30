0dd46d4f1066eb134b7c628223eed26e836b2e88
aldenpark@gmail.com <aldenpark@gmail.com>
2020-07-30 15:32:13 +0000

    config change

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_9EVRhaOvv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '22mm3g1fadc2v11jv4g5fa5205', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
